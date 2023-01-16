fde1f76967ec966331a461f240991dbe301138d5
z4cope <63551733+z4cope@users.noreply.github.com>
2023-01-17 01:10:04 +0200

    Change db name & line 3 query in query.sql

--- data.sql
+++ data.sql
@@ -1,6 +1,6 @@
 /* Populate database with sample data. */
 
-INSERT INTO vet_clinic(name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES('Agumon', '2020-2-3', 0, TRUE, 10.23);
-INSERT INTO vet_clinic(name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES('Gabumon', '2018-11-15', 2, TRUE, 8);
-INSERT INTO vet_clinic(name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES('Pikachu', '2021-6-12', 1, FALSE, 15.04);
-INSERT INTO vet_clinic(name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES('Devimon', '2017-5-12', 5, TRUE, 11);
+INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES('Agumon', '2020-2-3', 0, TRUE, 10.23);
+INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES('Gabumon', '2018-11-15', 2, TRUE, 8);
+INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES('Pikachu', '2021-6-12', 1, FALSE, 15.04);
+INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES('Devimon', '2017-5-12', 5, TRUE, 11);
--- queries.sql
+++ queries.sql
@@ -1,10 +1,10 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * FROM vet_clinic WHERE name LIKE '%mon%';
-SELECT * FROM vet_clinic WHERE date_of_birth BETWEEN '2016-1-1' AND '2019-1-1';
-SELECT * FROM vet_clinic WHERE escape_attempts < 3;
-SELECT date_of_birth FROM vet_clinic WHERE name = 'Agumon' OR name = 'Pikachu';
-SELECT name, escape_attempts FROM vet_clinic WHERE weight_kg > 10.5;
-SELECT * FROM vet_clinic WHERE neutered = TRUE;
-SELECT * FROM vet_clinic WHERE name != 'Gabumon';
-SELECT * FROM vet_clinic WHERE weight_kg <= 10.4 AND weight_kg <= 17.3;
+SELECT * FROM animals WHERE RIGHT(name,3) = 'mon';
+SELECT * FROM animals WHERE date_of_birth BETWEEN '2016-1-1' AND '2019-1-1';
+SELECT * FROM animals WHERE escape_attempts < 3;
+SELECT date_of_birth FROM animals WHERE name = 'Agumon' OR name = 'Pikachu';
+SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
+SELECT * FROM animals WHERE neutered = TRUE;
+SELECT * FROM animals WHERE name != 'Gabumon';
+SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
